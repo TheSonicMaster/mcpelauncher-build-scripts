d0144bd6ceca49aa4f0f246174ad500df4b358b8
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-04-25 13:18:10 +0100

    Verify source sha256sum after download

--- build-deb.sh
+++ build-deb.sh
@@ -83,7 +83,7 @@ status "==> Downloading source code..."
 curl -O https://www.thesonicmaster.net/software/mcpelauncher-thesonicmaster/source/mcpelauncher-thesonicmaster-$ver.tar.xz
 # Verify source sha256sum.
 status2 "==> Verifying source against sha256sum... "
-curl -O https://www.thesonicmaster.net/software/mcpelauncher-thesonicmaster/source/mcpelauncher-thesonicmaster-$ver.tar.xz.sha256 | sha256sum -c > /dev/null
+curl -s https://www.thesonicmaster.net/software/mcpelauncher-thesonicmaster/source/mcpelauncher-thesonicmaster-$ver.tar.xz.sha256 | sha256sum -c > /dev/null
 status "All Good!"
 # Extract source tarball.
 status "==> Unpacking source tarball, please be patient..."
