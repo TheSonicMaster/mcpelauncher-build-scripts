1bc0593eb6d06cd3020a9330b2fe248e8bdaa721
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-05-15 14:47:42 +0100

    Ensure $ARCH is set

--- build-appimage.sh
+++ build-appimage.sh
@@ -193,7 +193,7 @@ END
 status "==> Building AppImage..."
 export ARCH=$arch
 export OUTPUT="mcpelauncher-thesonicmaster-$ver-$arch.AppImage"
-linuxdeploy/squashfs-root/AppRun --appdir $appdir --output appimage
+ARCH=$arch linuxdeploy/squashfs-root/AppRun --appdir $appdir --output appimage
 mv mcpelauncher-thesonicmaster-$ver-$arch.AppImage "$savedir"
 status "==> Cleaning up..."
 rm -rf $builddir $pkgdir $lddir
